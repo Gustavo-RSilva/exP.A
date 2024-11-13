b627f3d8b113fde0544cc919b2acbf615ceb2ec1
Gustavo.RSilva <gu222498@gmail.com>
2024-11-13 11:31:26 -0300

    Adicionar arquivos de projeto.

---
+++ P.A/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P.A
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            listBox1.Items.Clear();
+
+
+            int primeiroTermo = int.Parse(textBox1.Text);
+            int razao = int.Parse(textBox2.Text);
+
+
+            int numeroDeTermos = 10;
+            int contador = 0;
+
+
+            while (contador < numeroDeTermos)
+            {
+                int termoAtual = primeiroTermo + contador * razao;
+                listBox1.Items.Add(termoAtual);
+                contador++;
+            }
+
+        }
+    }
+}
